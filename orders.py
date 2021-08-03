e8f51a7f7f678c2ccee54ad0b2fdfffd042656bb
FataHoxha <oxa.fat@gmail.com>
2021-08-03 18:40:36 +0200

    convert xml to json

--- orders.py
+++ orders.py
@@ -1,4 +1,17 @@
 import requests
+import xmltodict,json
+
+# 1. open xml and convert to json in order to easly handle the data structure
+with open('employee_orders.xml', 'r') as xmlfile:
+    json_res= xmltodict.parse(xmlfile.read())
+
+# check if result is correct
+#print(json.dumps(json_res))
+
+# 2. format the order in a the employee orders in the desired format for request body
+
+
+# 3. send the request
 
 url = 'https://nhna.com/api/v1/bulk/order'
 orders = {
